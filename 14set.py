8e176e6949420b4d802775dade83ac5f99eac43b
DIVYANGSHR <divyangshrivastav1998@gmail.com>
2025-01-22 18:31:38 +0530

    paython practise

---
+++ 14set.py
@@ -0,0 +1,42 @@
+# # set1 = {1, 2, 3,'ramu',9}
+
+# # set2 = set([3, 4, 5,'ramu',6]) 
+ 
+# # print(set1)
+# # print(set2)
+
+# # print()
+
+# # #empty ----------======
+
+# # empty_set = set()
+# # print(empty_set)
+
+# # print()
+
+# # unique set==========
+
+# # unique_set = {1,2,3,4,5,6,7,8,9,}
+# # print(unique_set)
+
+# # print()
+
+# # unorder set =============
+
+# my_set = {1,2,5,4,6,9,8,3}
+# print(my_set)
+
+# print()
+
+# ADD  set  =============
+
+my_set = {1,2,3,5}
+
+my_set.add(4)
+
+print(my_set)
+
+
+
+
+
