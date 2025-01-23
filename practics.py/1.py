79951a622980a3d4138cd1497d277ab07e9a19d6
DIVYANGSHR <divyangshrivastav1998@gmail.com>
2025-01-23 17:09:07 +0530

    practce

--- practics.py/1.py
+++ practics.py/1.py
@@ -1,25 +1,23 @@
-# #    ====== Print Statement   =====
+# # # # #    ====== Print Statement   =====
+print("hello")
 
-# print("hello")
+print(" Hello,world")
 
-# # ======= 1 ======
-# print(" Hello,world")
+ # # ======= 2 ==========
 
-# # ======= 2 ==========
-
-# print("india 1 ")
-# print("mumbai 2")
-# print("uttar pradesh 3")
+print("india 1 ")
+print("mumbai 2")
+print("uttar pradesh 3")
 
 # # ========= 3 ======
 
-# print("hello \n i am \n divyang")
+print("hello \n i am \n divyang")
 
 
-# #=== 4 ================
-# var1 = "Hello"
-# var2 = "namste"
-# print(var1 + " " + var2)
+ #=== 4 ================
+var1 = "Hello"
+var2 = "namste"
+print(var1 + " " + var2)
 
 
 # =====   5 ======
@@ -36,45 +34,74 @@ print()
 print('"kalyan"')
 
 
-#=========== typing castinf ==========
+# # #=========== typing castinf ==========
 
-#=============== 1 ==========
+# # #=============== 1 ==========
 
 int_value = 42
 float_value = float(int_value)
 print(float_value)  
 
-#====== 2 ==========
+# # #====== 2 ==========
 
 float_value = 22.18
 int_value = int(float_value)
 print(int_value) 
 
-#---- ==  3 ===
+# # #---- ==  3 ===
 
 str_num = "20"
 print(int(str_num))
 
-#======= 4 ==========
+# # #======= 4 ==========
 str_num = "15.5"
 print(float(str_num))
 
-#-========= 5 =====
+# # #-========= 5 =====
 num = 100
 print(str(num))
 
-#---==== 6 =====
+# # #---==== 6 =====
 num = 10.5
 print(str(num))
 
-#======= 7 ======
+# # #======= 7 ======
 binary_str = "1010"
 print(int(binary_str, 2))
 
 dat=("enter the month")
 
+# # --- assiment 3======
+r = 20
+b = 40
+c = 15
+
+if r < b and r < c:
+     print("Ram")
+elif b < r and b < c:
+     print("Shyam")
+else:
+    print("Ajay")
+
+# #-------- 4==============
+
+m1 = 70
+m2 = 50
+m3 = 80
 
 
+total_marks = m1 + m2 + m3
+
+percentage = (total_marks / 300) * 100
+
+if percentage > 80:
+    print("A")
+elif percentage >= 65:
+    print("B")
+elif percentage >= 45:
+    print("C")
+else:
+   print("D")
 
 
 
---
+++ practics.py/18 dict.py
@@ -0,0 +1,71 @@
+# empt_dic = {}
+# print(empt_dic)
+
+# print()
+
+# #--- dic syntex ===================
+
+# my_dic = {'name': 'rohan', 'gender': 'male' , 'age': '28', 'addresh':'kalyan'}
+
+# print(my_dic)
+
+# print()
+
+# #---- dic values ================
+
+# strings_values = {'name':'radha','gender': 'female' , 'age': '24','addresh':'badlapur'}
+
+# list_values ={'numbers':[1,2,3] , 'colours':['red','black','green']}
+
+# print(strings_values)
+# print(list_values)
+
+
+# #========== over right ============
+
+# strings_values  ['name'] = 'jolly'
+# print(strings_values)
+
+# print()
+
+# strings_values ['name'] = 'don'
+
+# print(strings_values)
+
+# print()
+
+# # ----- Update method -----
+
+# dict1 = {'a': 2, 'b': 3 ,'c':4}
+
+# dict2 = {'b': 5, 'e': 6}
+
+# dict1.update(dict2)
+
+# dict1.update({'c':'7'})
+
+# print(dict1)
+
+# print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
