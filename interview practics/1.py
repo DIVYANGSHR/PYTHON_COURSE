5906514f97d2379b74e13431a576d402567c34ce
DIVYANGSHR <divyangshrivastav1998@gmail.com>
2025-02-11 14:57:06 +0530

    practise

---
+++ interview practics/1.py
@@ -0,0 +1,15 @@
+# Write a Python program to print "Hello Python".
+
+./      #output  Hello Divyang     
+
+#Write a Python program to do arithmetical operations addition and division.
+#ADDITION
+# num1 = float(input("Enter the first number for addition:"))  #OUTPUT
+# num2 = float(input("Enter the second number for addition:")) # Enter the first number for addition:9
+# sum_result = num1 + num2                                     #Enter the second number for addition: 12
+# print(f"sum: {num1} + {num2} = {sum_result}")                #sum: 9.0 + 12.0 = 21.0
+
+
+
+
+
